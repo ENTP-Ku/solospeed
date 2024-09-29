48075fbf9ccfcdadfb23fd1a94e9b65e0f4afa93
GuJauk <wkdnr1254@gmail.com>
2024-09-29 15:52:27 +0900

    날짜 포맷

--- demo/src/main/java/com/example/demo/Post.java
+++ demo/src/main/java/com/example/demo/Post.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import jakarta.persistence.*;
 import java.util.Date;
 
+
 @Entity
 @Table(name = "posts")
 @Getter
@@ -19,24 +20,22 @@ public class Post {
     private Long id;
 
     private String title;
-
     private String content;
-    
     private String username; // 작성자 username
     
     @Temporal(TemporalType.TIMESTAMP)
-    private Date createdAt = new Date(); // 기본값으로 현재 시간 설정
-    
+    private Date createdAt; // 기본값으로 현재 시간 설정
+
     @Temporal(TemporalType.TIMESTAMP)
     private Date updatedAt; // 수정 시 설정될 값
 
     @PrePersist
     protected void onCreate() {
-        createdAt = new Date();
+        this.createdAt = new Date(); // 현재 시간으로 설정
     }
 
     @PreUpdate
     protected void onUpdate() {
-        updatedAt = new Date();
+        this.updatedAt = new Date(); // 현재 시간으로 설정
     }
 }
